36aeaf97fc3d62f1f7f40107ade2234e2691d00d
jaeheon.na <jaeheon.na@jaeheonna-MacBook-Pro.local>
2025-10-30 11:10:44 +0900

    ch11: string

---
+++ ch11(자료구조-딕셔너리,문자열)/ex05.py
@@ -0,0 +1,12 @@
+word = "Python"
+word1 = word[0:3]
+print(word1)
+word2 = word[0:100]
+print(word2)
+print("--" * 10)
+
+print("apple" < "banana")
+print(ord('a'))
+print(ord('b'))
+print(chr(97))
+print(chr(98))
