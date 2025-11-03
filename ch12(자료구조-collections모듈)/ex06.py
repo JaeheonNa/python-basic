6d96b5f6e5c57652ab5741dbbdf9a940854defd9
jaeheonNa <jaeheon.na@hlcompany.com>
2025-11-03 11:06:49 +0900

    ch12: DefaultDictionary

---
+++ ch12(자료구조-collections모듈)/ex06.py
@@ -0,0 +1,40 @@
+def countLetters(words):
+    counter = {}
+    for word in words:
+        if word in counter:
+            counter[word] += 1
+        else:
+            counter[word] = 1
+    return counter
+
+
+def countLetters_setDefault(words):
+    counter = {}
+    for word in words:
+        counter.setdefault(word, 0)
+        counter[word] += 1
+    return counter
+
+from collections import defaultdict
+
+def countLetters_defaultdict(words):
+    counter = defaultdict(int)
+    for word in words:
+        counter[word] += 1
+    return counter
+
+
+if __name__ == '__main__':
+    words = 'apple'
+
+    dic = countLetters(words)
+    print(dic.items())
+    print('--' * 10)
+
+    dic = countLetters_setDefault(words)
+    print(dic.items())
+    print('--' * 10)
+
+    dic = countLetters_defaultdict(words)
+    print(dic.items())
+    print('--' * 10)
