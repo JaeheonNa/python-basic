5fa75c4d40250eecd6f965524b3004b2036abdec
jaeheon.na <jaeheon.na@jaeheonna-MacBook-Pro.local>
2025-11-06 20:14:34 +0900

    ch14

---
+++ ch14(객체지향프로그래밍-2)/Rectangle.py
@@ -0,0 +1,33 @@
+class Rectangle:
+    __height = 0
+    __width = 0
+    __area = 0
+
+    def __init__(self, height, width):
+        self.__height = height
+        self.__width = width
+        self.calcArea()
+
+    def calcArea(self):
+        self.__area = self.__height*self.__width
+
+    def setHeight(self, height):
+        self.__height = height
+        self.calcArea()
+
+    def setWidth(self, width):
+        self.__width = width
+        self.calcArea()
+
+    def getHeight(self):
+        return self.__height
+    def getWidth(self):
+        return self.__width
+    def getArea(self):
+        return self.__area
+
+    def __eq__(self, other):
+        return self.__height == other.getHeight() and self.__width == other.getWidth() and self.__area == other.getArea()
+
+    def __str__(self):
+        return "높이: %d, 너비: %d, 넓이: %d" % (self.__height, self.__width, self.__area)
---
+++ ch14(객체지향프로그래밍-2)/ex01.py
@@ -0,0 +1,15 @@
+from Rectangle import *
+
+def increaseHeight(rect):
+    rect.setHeight(rect.getHeight() + 1)
+
+if __name__ == '__main__':
+    rectangle1 = Rectangle(10, 5)
+    print("rectangle1: ",rectangle1.__str__())
+    increaseHeight(rectangle1)
+
+    rectangle2 = Rectangle(11, 5)
+    print("rectangle1: ",rectangle1.__str__())
+    print("rectangle2: ",rectangle2.__str__())
+
+    print("rectangle1 == rectangle2: ", rectangle1.__eq__(rectangle2))
