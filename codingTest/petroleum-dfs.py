44cc987e5aee00e7ae6e236d650b1427965ced31
jaeheonNa <jaeheon.na@hlcompany.com>
2025-12-13 09:35:54 +0900

    codingTest: petroleum

--- codingTest/petroleum-dfs.py
+++ codingTest/petroleum-dfs.py
@@ -1,16 +1,17 @@
 from collections import deque
 import copy
 
-def checkPetroleum(drilling):
+def checkPetroleum(drilling, trialLand, oilId, oilIdCntMap):
     petroleumCnt = 0
+    oil = []
+
     while len(drilling) > 0 :
         site = drilling.popleft()
-        trialLand = site[0]
-        depth = site[1]
-        trial = site[2]
-        if trialLand[depth][trial] == 1:
+        depth = site[0]
+        trial = site[1]
+        if trialLand[depth][trial] == 1 and (depth, trial) not in oil:
             petroleumCnt += 1
-            trialLand[depth][trial] = 0
+            oil.append((depth, trial))
             if len(trialLand) > depth + 1:
                 drilling.append([trialLand, depth + 1, trial])
             if 0 <= depth - 1:
@@ -19,22 +20,29 @@ def checkPetroleum(drilling):
                 drilling.append([trialLand, depth, trial + 1])
             if 0 <= trial - 1:
                 drilling.append([trialLand, depth, trial - 1])
-    return petroleumCnt
+
+    for depth, trial in oil:
+        trialLand[depth][trial] = oilId
+    oilIdCntMap[oilId] = petroleumCnt
 
 
 def solution(land):
     answer = 0
+    oilId = 1
     trialCnt = len(land[0])
+    oilIdCntMap = {}
     for trial in range(trialCnt):
         trialLand = copy.deepcopy(land)
-        petroleumCnt = 0
         for depth in range(len(trialLand)):
             if trialLand[depth][trial] == 1:
+                oilId += 1
                 drilling = deque()
-                drilling.append([trialLand, depth, trial])
-                petroleumCnt += checkPetroleum(drilling)
-        if petroleumCnt > answer:
-            answer = petroleumCnt
+                drilling.append([depth, trial])
+                checkPetroleum(drilling, trialLand, oilId, oilIdCntMap)
+
+
+
+
 
     return answer
 
