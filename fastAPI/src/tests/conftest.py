d39e8d69dcab65867f29e2cf44ceb46d73bbdd86
jaeheonNa <jaeheon.na@hlcompany.com>
2026-01-09 14:52:28 +0900

    clear cached

--- fastAPI/src/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from fastapi.testclient import TestClient
-from main import app
-
-
-# 이렇게 해놓으면, pytest를 수행할 함수에 client 인자만 넣어주면 됨.
-@pytest.fixture
-def test_client():
-    return TestClient(app=app)
